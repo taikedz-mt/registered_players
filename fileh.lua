9c3d0f385d4b5cfebf12d235c1956d4b572aa7b0
Tai @ Flex <dch.tai@gmail.com>
2017-06-29 18:37:56 +0100

    flesh out file handlers

--- fileh.lua
+++ fileh.lua
@@ -1,1 +1,13 @@
 pregistry.file = {}
+
+function pregistry.file:removeban(playername)
+end
+
+function pregistry.file:addban(playername)
+end
+
+local function load_bans()
+end
+
+local function save_bans()
+end
